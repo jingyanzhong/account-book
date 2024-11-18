f8d6ff7c88ec447511d6188db270fa7525d8d6dd
jingyan0721 <melody804210@gmail.com>
2024-11-18 11:19:17 +0800

    [ main ]調整journal_cash頁面，加入debit select選項

--- src/main/webapp/vue/src/components/modelComponent.vue
+++ src/main/webapp/vue/src/components/modelComponent.vue
@@ -1,143 +1,170 @@
 <script setup>
-import { ref, watch } from 'vue'
-import { useModelShowStore } from '@/stores/modelShow';
-import { storeToRefs } from 'pinia';
-const ModelShowStore = useModelShowStore();
-const { isShowModel, isShowMask } = ModelShowStore;
+import { ref, watch, onMounted } from 'vue'
+import { useModelShowStore } from '@/stores/modelShow'
+import { storeToRefs } from 'pinia'
+const ModelShowStore = useModelShowStore()
+const { isShowModel, isShowMask } = ModelShowStore
 const { showModel, isNew } = storeToRefs(ModelShowStore)
+import axios from 'axios'
 const emit = defineEmits(['update-list', 'edit-list'])
 const props = defineProps({
-    list: {
-        type: Object,
-        default: {}
-    }
+  list: {
+    type: Object,
+    default: {},
+  },
+  debitData: {
+    type: Object,
+    default: {},
+  },
 })
-watch(() => props.list,
-        (item) => {
-            cashItem.value = item
-    }
+watch(
+  () => props.list,
+  (item) => {
+    cashItem.value = item
+  },
 )
-const cashItem = ref({})
+watch(
+  () => props.debitData,
+  (item) => {
+    debitItem.value = item
+  },
+)
 
-function updateItem(val, {resetForm}) {
-    isShowMask(false);
-    isShowModel(false);
-    if(isNew.value) {
-        emit('update-list', cashItem);
-    }else {
-        emit('edit-list', cashItem);
-    }
-    resetForm()
+const cashItem = ref({})
+const debitItem = ref({})
+function updateItem(val, { resetForm }) {
+  isShowMask(false)
+  isShowModel(false)
+
+  if (isNew.value) {
+    emit('update-list', cashItem.value)
+  } else {
+    emit('edit-list', cashItem.value)
+  }
+  resetForm()
 }
 </script>
 <template>
-    <div class="model" :class="{'active' : showModel}">
+  <div class="model" :class="{ active: showModel }">
     <div class="header">
-        <h3>JOURNAL / CASH
-            <span v-if="isNew">新增</span>
-            <span v-else>編輯</span>
-        </h3>
+      <h3>
+        JOURNAL / CASH
+        <span v-if="isNew">新增</span>
+        <span v-else>編輯</span>
+      </h3>
     </div>
     <div class="body">
-        <v-form v-slot="{ errors }" @submit="updateItem">
-            <div class="input_group">
-                <div class="input_item">
-                    <label>
-                        Date <br>
-                        <v-field type="date"
-                        rules="required"
-                        name="日期"  v-model="cashItem.date" :disabled="!isNew"
-                        :class="{ 'is-invalid': errors['日期'] }"></v-field>
-                        <error-message name="日期" class="invalid-feedback"></error-message>
-                    </label>
-                </div>
-                <div class="input_item">
-                    <label class="amount_input">
-                        Amount <br>
-                        <v-field type="number"
-                        rules="required|min:1"
-                        name="金額" placeholder="請輸入金額..." v-model="cashItem.amount"
-                        :class="{ 'is-invalid': errors['金額'] }"></v-field>
-                        <error-message name="金額" class="invalid-feedback"></error-message>
-                    </label>
-                </div>
-            </div>
-            <div class="input_group">
-                <div class="input_item">
-                    <label>
-                        Debit <br>
-                        <v-field as="select" name="debit"
-                        rules="required" v-model="cashItem.debit"
-                        :class="{ 'is-invalid': errors.debit }">
-                            <option value="">請選擇...</option>
-                            <option value="早餐">早餐</option>
-                        </v-field>
-                        <error-message name="debit" class="invalid-feedback"></error-message>
-                    </label>
-                </div>
-                <div class="input_item">
-                    <label>
-                        Credit <br>
-                        <v-field as="select" name="credit" 
-                        rules="required"v-model="cashItem.credit"
-                        :class="{ 'is-invalid': errors.credit }">
-                            <option value="">請選擇...</option>
-                            <option value="現金">現金</option>
-                            <option value="信用卡">信用卡</option>
-                        </v-field>
-                        <error-message name="credit" class="invalid-feedback"></error-message>
-                    </label>
-                </div>
-            </div>
-            <div class="input_group">
-                <div class="input_item">
-                    <label>
-                        Items <br>
-                        <v-field type="text" name="項目" 
-                        rules="required|min:1"placeholder="請輸入項目..." v-model="cashItem.item"
-                        :class="{ 'is-invalid': errors['項目'] }"></v-field>
-                        <error-message name="項目" class="invalid-feedback"></error-message>
-                    </label>
-                </div>
-                <div class="input_item">
-                    <label>
-                        Place <br>
-                        <v-field type="text" name="地點" 
-                        rules="required|min:1"placeholder="請輸入地點..." v-model="cashItem.place"
-                        :class="{ 'is-invalid': errors['地點'] }"></v-field>
-                        <error-message name="地點" class="invalid-feedback"></error-message>
-                    </label>
-                </div>
-            </div>
-            <div class="input_group">
-                <div class="input_item">
-                    <label>
-                        Account <br>
-                        <v-field as="select" name="帳號" 
-                        rules="required" v-model="cashItem.account"
-                        :class="{ 'is-invalid': errors['帳號'] }">
-                            <option value="">請選擇帳號...</option>
-                            <option value="jingyan">Jingyan</option>
-                        </v-field>
-                        <error-message name="帳號" class="invalid-feedback"></error-message>
-                    </label>
-                </div>
-                <div class="input_item">
-                    <label for="">
-                        ID
-                        <v-field type="number" name="id"
-                        rules="required|min:4|max:4" placeholder="請輸入4位數字" v-model="cashItem.id" :disabled="!isNew"
-                        :class="{ 'is-invalid': errors.id }"></v-field>
-                        <error-message name="id" class="invalid-feedback"></error-message>
-                    </label>
-                </div>
-            </div>
-            <div class="btn-group">
-                <button type="reset" class="cancel" @click="isShowMask(false); isShowModel(false)">取消</button>
-                <button v-if="isNew" type="submit" class="add" >新增</button>
-                <button v-else type="submit" class="add" >編輯</button>
-            </div>
-        </v-form>
+      <v-form v-slot="{ errors, meta, resetForm }" @submit="updateItem">
+        <div class="input_group">
+          <div class="input_item">
+            <label>
+              Date <br />
+              <v-field
+                type="date"
+                rules="required"
+                name="日期"
+                v-model="cashItem.date"
+                :disabled="!isNew"
+                :class="{ 'is-invalid': errors['日期'] }"
+              ></v-field>
+              <error-message name="日期" class="invalid-feedback"></error-message>
+            </label>
+          </div>
+          <div class="input_item">
+            <label class="amount_input">
+              Amount <br />
+              <v-field
+                type="number"
+                rules="required|min:1"
+                name="金額"
+                placeholder="請輸入金額..."
+                v-model="cashItem.amount"
+                :class="{ 'is-invalid': errors['金額'] }"
+              ></v-field>
+              <error-message name="金額" class="invalid-feedback"></error-message>
+            </label>
+          </div>
+        </div>
+        <div class="input_group">
+          <div class="input_item">
+            <label>
+              Debit <br />
+              <v-field
+                as="select"
+                name="debit"
+                rules="required"
+                v-model="cashItem.debit"
+                :class="{ 'is-invalid': errors.debit }"
+              >
+                <option value="">請選擇...</option>
+                <option v-for="item in debitItem" :value="item.code" :key="item.code">
+                  {{ item.name }}
+                </option>
+              </v-field>
+              <error-message name="debit" class="invalid-feedback"></error-message>
+            </label>
+          </div>
+          <div class="input_item">
+            <label>
+              Credit <br />
+              <v-field
+                as="select"
+                name="credit"
+                rules="required"
+                v-model="cashItem.credit"
+                :class="{ 'is-invalid': errors.credit }"
+              >
+                <option value="">請選擇...</option>
+                <option value="現金">現金</option>
+                <option value="信用卡">信用卡</option>
+              </v-field>
+              <error-message name="credit" class="invalid-feedback"></error-message>
+            </label>
+          </div>
+        </div>
+        <div class="input_group">
+          <div class="input_item">
+            <label>
+              MEMO <br />
+              <v-field
+                type="text"
+                name="MEMO"
+                rules="required|min:1"
+                placeholder="請輸入摘要..."
+                v-model="cashItem.item"
+                :class="{ 'is-invalid': errors['MEMO'] }"
+              ></v-field>
+              <error-message name="MEMO" class="invalid-feedback"></error-message>
+            </label>
+          </div>
+          <div class="input_item">
+            <label for="">
+              ID
+              <v-field
+                type="number"
+                name="id"
+                rules="required|min:4|max:4"
+                placeholder="請輸入4位數字"
+                v-model="cashItem.id"
+                :disabled="!isNew"
+                :class="{ 'is-invalid': errors.id }"
+              ></v-field>
+              <error-message name="id" class="invalid-feedback"></error-message>
+            </label>
+          </div>
+        </div>
+        <div class="btn-group">
+          <button
+            type="button"
+            class="cancel"
+            @click="isShowMask(false), isShowModel(false), resetForm()"
+          >
+            取消
+          </button>
+          <button v-if="isNew" type="submit" class="add" :disabled="!meta.valid">新增</button>
+          <button v-else type="submit" class="add" :disabled="!meta.valid">編輯</button>
+        </div>
+      </v-form>
     </div>
-</div>
+  </div>
 </template>
--- src/main/webapp/vue/src/views/SubjectsView.vue
+++ src/main/webapp/vue/src/views/SubjectsView.vue
@@ -5,162 +5,182 @@ const debitItem = ref({})
 const creditItem = ref({})
 
 function addDebitItem(values, { resetForm }) {
-    console.log('d',values);
-    resetForm();
+  console.log('d', values)
+  resetForm()
 }
 
 function addCreditItem(values, { resetForm }) {
-    console.log('c', values);
-    resetForm();
+  console.log('c', values)
+  resetForm()
 }
 </script>
 
 <template>
-    <div class="bg-gray">
-        <div class="container container-subject">
-            <div class="title">
-                <h2>SUBJECT
-                    <span> | 會計科目</span>
-                </h2>
+  <div class="bg-gray">
+    <div class="container container-subject">
+      <div class="title">
+        <h2>
+          SUBJECT
+          <span> | 會計科目</span>
+        </h2>
+      </div>
+      <div class="btn-group">
+        <button
+          class="subject-btn"
+          :class="{ active: subject === 'debit' }"
+          @click="subject = 'debit'"
+        >
+          Debit
+        </button>
+        <button
+          class="subject-btn"
+          :class="{ active: subject === 'credit' }"
+          @click="subject = 'credit'"
+        >
+          Credit
+        </button>
+      </div>
+      <div class="subject_content debit" v-if="subject === 'debit'">
+        <div class="add_subject">
+          <h3>新增Debit</h3>
+          <v-form id="form" ref="dform" v-slot="{ errors, meta, resetForm }" @submit="addDebitItem">
+            <div class="input_item">
+              <label>
+                代號 <br />
+                <v-field
+                  type="number"
+                  name="代號"
+                  rules="required|max:4|min:4"
+                  placeholder="請輸入4位數字代號"
+                  v-model="debitItem.CODE"
+                  :class="{ 'is-invalid': errors['代號'] }"
+                ></v-field>
+                <error-message name="代號" class="invalid-feedback"></error-message>
+              </label>
+            </div>
+            <div class="input_item">
+              <label for="">
+                名稱 <br />
+                <v-field
+                  type="text"
+                  placeholder="請輸入名稱"
+                  name="名稱"
+                  rules="required"
+                  :class="{ 'is-invalid': errors['名稱'] }"
+                  v-model="debitItem.NAME"
+                ></v-field>
+                <error-message name="名稱" class="invalid-feedback"></error-message>
+              </label>
             </div>
             <div class="btn-group">
-                <button class="subject-btn" :class="{'active' : subject === 'debit'}" @click="subject = 'debit'">Debit</button>
-                <button class="subject-btn" :class="{'active' : subject === 'credit'}" @click="subject = 'credit'">Credit</button>
+              <button type="button" class="cancel" @click="resetForm">取消</button>
+              <button type="subimt" class="add" :disabled="!meta.valid">新增</button>
             </div>
-            <div class="subject_content debit" v-if="subject === 'debit'">
-                <div class="add_subject">
-                    <h3>新增Debit</h3>
-                    <v-form id="form" ref="dform" v-slot="{ errors }" @submit="addDebitItem">
-                        <div class="input_item">
-                            <label>
-                                代號 <br>
-                                <v-field type="number" 
-                                name="代號" 
-                                rules="required|max:4|min:4" 
-                                placeholder="請輸入4位數字代號" 
-                                v-model="debitItem.CODE"
-                                :class="{ 'is-invalid': errors['代號'] }"></v-field>
-                                <error-message name="代號" class="invalid-feedback"></error-message>
-                            </label>
-                        </div>
-                        <div class="input_item">
-                            <label for="">
-                                名稱 <br>
-                                <v-field type="text"
-                                 placeholder="請輸入名稱"
-                                 name="名稱"
-                                 rules="required"
-                                 :class="{ 'is-invalid': errors['名稱'] }"
-                                 v-model="debitItem.NAME"></v-field>
-                                 <error-message name="名稱" class="invalid-feedback"></error-message>
-                            </label>
-                        </div>
-                        <div class="btn-group">
-                            <button type="reset" class="cancel">取消</button>
-                            <button type="subimt" class="add" >新增</button>
-                        </div>
-                    </v-form>
-                </div>
-                <div class="subject_table">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th width="15%">代號</th>
-                                <th width="30%">名稱</th>
-                                <th width="23%">建立時間</th>
-                                <th width="32%"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    1001
-                                </td>
-                                <td>薪水</td>
-                                <td>2024-11-12 22:07:00</td>
-                                <td>
-                                    <div class="btn-group">
-                            <button class="edit">
-                                <span class="material-symbols-outlined">edit</span>
-                            </button>
-                            <button class="del">
-                                <span class="material-symbols-outlined">delete</span>
-                            </button>
-                        </div>
-                                </td>
-                            </tr>
-                            
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div class="subject_content credit" v-else>
-                <div class="add_subject">
-                    <h3>新增Credit</h3>
-                    <v-form id="form" ref="Cform" v-slot="{ errors }" @submit="addCreditItem">
-                        <div class="input_item">
-                            <label>
-                                代號 <br>
-                                <v-field type="number" 
-                                name="Credit代號" 
-                                rules="required|max:4|min:4" 
-                                placeholder="請輸入4位數字代號" 
-                                v-model="creditItem.CODE"
-                                :class="{ 'is-invalid': errors['Credit代號'] }"></v-field>
-                                <error-message name="Credit代號" class="invalid-feedback"></error-message>
-                            </label>
-                        </div>
-                        <div class="input_item">
-                            <label for="">
-                                名稱 <br>
-                                <v-field type="text"
-                                 placeholder="請輸入名稱"
-                                 name="Credit名稱"
-                                 rules="required"
-                                 :class="{ 'is-invalid': errors['Credit名稱'] }"
-                                 v-model="creditItem.NAME"></v-field>
-                                 <error-message name="Credit名稱" class="invalid-feedback"></error-message>
-                            </label>
-                        </div>
-                        <div class="btn-group">
-                            <button type="reset" class="cancel">取消</button>
-                            <button type="subimt" class="add" >新增</button>
-                        </div>
-                    </v-form>
-                </div>
-                <div class="subject_table">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th width="15%">代號</th>
-                                <th width="30%">名稱</th>
-                                <th width="23%">建立時間</th>
-                                <th width="32%"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    1001
-                                </td>
-                                <td>現金</td>
-                                <td>2024-11-12 22:07:00</td>
-                                <td>
-                                    <div class="btn-group">
-                            <button class="edit">
-                                <span class="material-symbols-outlined">edit</span>
-                            </button>
-                            <button class="del">
-                                <span class="material-symbols-outlined">delete</span>
-                            </button>
-                        </div>
-                                </td>
-                            </tr>
-                            
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+          </v-form>
         </div>
+        <div class="subject_table">
+          <table>
+            <thead>
+              <tr>
+                <th width="15%">代號</th>
+                <th width="30%">名稱</th>
+                <th width="23%">建立時間</th>
+                <th width="32%"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1001</td>
+                <td>薪水</td>
+                <td>2024-11-12 22:07:00</td>
+                <td>
+                  <div class="btn-group">
+                    <button class="edit">
+                      <span class="material-symbols-outlined">edit</span>
+                    </button>
+                    <button class="del">
+                      <span class="material-symbols-outlined">delete</span>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="subject_content credit" v-else>
+        <div class="add_subject">
+          <h3>新增Credit</h3>
+          <v-form
+            id="form"
+            ref="Cform"
+            v-slot="{ errors, meta, resetForm }"
+            @submit="addCreditItem"
+          >
+            <div class="input_item">
+              <label>
+                代號 <br />
+                <v-field
+                  type="number"
+                  name="Credit代號"
+                  rules="required|max:4|min:4"
+                  placeholder="請輸入4位數字代號"
+                  v-model="creditItem.CODE"
+                  :class="{ 'is-invalid': errors['Credit代號'] }"
+                ></v-field>
+                <error-message name="Credit代號" class="invalid-feedback"></error-message>
+              </label>
+            </div>
+            <div class="input_item">
+              <label for="">
+                名稱 <br />
+                <v-field
+                  type="text"
+                  placeholder="請輸入名稱"
+                  name="Credit名稱"
+                  rules="required"
+                  :class="{ 'is-invalid': errors['Credit名稱'] }"
+                  v-model="creditItem.NAME"
+                ></v-field>
+                <error-message name="Credit名稱" class="invalid-feedback"></error-message>
+              </label>
+            </div>
+            <div class="btn-group">
+              <button type="button" class="cancel" @click="resetForm">取消</button>
+              <button type="subimt" class="add" :disabled="!meta.valid">新增</button>
+            </div>
+          </v-form>
+        </div>
+        <div class="subject_table">
+          <table>
+            <thead>
+              <tr>
+                <th width="15%">代號</th>
+                <th width="30%">名稱</th>
+                <th width="23%">建立時間</th>
+                <th width="32%"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1001</td>
+                <td>現金</td>
+                <td>2024-11-12 22:07:00</td>
+                <td>
+                  <div class="btn-group">
+                    <button class="edit">
+                      <span class="material-symbols-outlined">edit</span>
+                    </button>
+                    <button class="del">
+                      <span class="material-symbols-outlined">delete</span>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
