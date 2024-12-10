2a8739c9a600572adcec72d2eac546becab40220
TiramiAsu <tiramisu0116@gmail.com>
2024-12-10 21:35:28 +0800

    [ main ] 增修支援跨域打 API 問題

--- src/main/java/com/accountbook/controller/base/ApiController.java
+++ src/main/java/com/accountbook/controller/base/ApiController.java
@@ -3,12 +3,15 @@ package com.accountbook.controller.base;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 /**
  * [API Controller]
  *
  * @author cano.su
  * @since 2022/07/31
  */
+@CrossOrigin
 public abstract class ApiController extends BaseController {
 
     @Override
